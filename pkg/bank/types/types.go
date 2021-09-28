861b21d6d2dca0126fee87631f4decb0048e7ba2
Nekruzi Kabirzoda <nekruz.kabir@gmail.com>
2021-09-28 11:11:34 +0500

    types

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,22 @@
+package types
+
+type Money int64
+
+type Currency string
+
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+type Card struct{
+	ID int
+	PAN string
+	Balance Money
+	MinBalance Money
+	Currency Currency
+	Color string
+	Name string
+	Active bool
+}
